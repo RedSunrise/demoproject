8c3ce3d437540262464bb725fbde212163b9678a
RedSunrise <red.sunrise2@gmail.com>
2024-02-20 00:06:12 +0300

    failed6

--- src/test/java/apitests/apiinfo/TownsTests.java
+++ src/test/java/apitests/apiinfo/TownsTests.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import static io.restassured.RestAssured.given;
 
 public class TownsTests {
-    //randomComment3
+    //randomComment4
     @ParameterizedTest
     @ArgumentsSource(MyArgumentsProvider.class)
     void url(String city, Integer pageId) {
